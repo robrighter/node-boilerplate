0dec1d2e90c79618b4c7a81c240a3818bbf4fda1
Robert Righter <robrighter@gmail.com>
2010-08-16 23:54:02 -0400

    Fixed typo in js

--- project/static/js/script.js
+++ project/static/js/script.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
    socket.connect();
     
    $('#sender').bind('click', function() {
-     socket.send("Message Sent on" + new Date());     
+     socket.send("Message Sent on " + new Date());     
    });
    
    socket.on('message', function(data){
