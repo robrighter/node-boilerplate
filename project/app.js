03d949571ce3477b9ddd6d782f95bcc3d1a55e8d
Robert Righter <robrighter@gmail.com>
2010-08-16 23:46:46 -0400

    Client-side js cleanup

--- project/app.js
+++ project/app.js
@@ -1,9 +1,10 @@
+//setup Dependencies
 require('../lib/setup').ext( __dirname + "/../lib").ext( __dirname + "/../lib/express/support");
 var connect = require('connect');
 var sys = require('sys');
 var io = require('socket.io-node');
 
-
+//Setup Express
 var server = require('express').createServer();
 server.configure(function(){
     server.set('views', __dirname + '/views');
@@ -11,39 +12,37 @@ server.configure(function(){
     server.use(server.router);
     server.use(connect.staticProvider('./static'));
 });
-
 var port = 8765;
 server.listen( port);
-console.log('Listening on http://0.0.0.0:' + port )
 
-server.get('/test', function(req,res){
-  res.render('index.ejs', {
-    locals : { 
-              header: 'This is a header'
-             ,footer: 'This is a footer'
-             ,title : 'A Test Page'
-             ,description: 'Description Here'
-             ,author: 'Your Name'
-             ,analyticssiteid: 'XXXXXXX'
-             ,info: 'And this is info' 
-            }
-  });
-  res.send("HELLO");
-});
-
+//Setup Socket.IO
 var io = io.listen(server);
-
 io.on('connection', function(client){
-	console.log('WE GOT A CONNECTION!!!!!!!');
-
+	console.log('Client Connected');
 	client.on('message', function(message){
-		console.log('Recieved Message:\n' + sys.inspect(message));
 		client.broadcast(message);
 		client.send(message);
-		console.log('Brodcasted the following message: ' + message);
 	});
-
 	client.on('disconnect', function(){
-		console.log('WE GOT A DISCONNECT!!!!!!!!!!');
+		console.log('Client Disconnected.');
 	});
 });
+
+console.log('Listening on http://0.0.0.0:' + port )
+
+
+//Routes
+server.get('/', function(req,res){
+  res.render('index.ejs', {
+    locals : { 
+              header: '#Header#'
+             ,footer: '#Footer#'
+             ,title : 'Page Title'
+             ,description: 'Page Description'
+             ,author: 'Your Name'
+             ,analyticssiteid: 'XXXXXXX' 
+            }
+  });
+});
+
+
