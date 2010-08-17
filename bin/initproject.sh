4fe58824f73b663a57fcf82c24f568cd8f30ebfa
Robert Righter <robrighter@gmail.com>
2010-08-17 09:35:08 -0400

    Added init script

---
+++ bin/initproject.sh
@@ -0,0 +1,12 @@
+echo "Copying Markup and CSS BoilerPlate..."
+cp ./lib/html5-boilerplate/404.html ./project/views/404.ejs
+cp ./lib/html5-boilerplate/js/plugins.js ./project/static/js/plugins.js
+cp ./lib/html5-boilerplate/css/handheld.css ./project/static/css/handheld.css
+cp ./lib/html5-boilerplate/css/style.css ./project/static/css/style.css
+cp ./lib/html5-boilerplate/robots.txt ./project/static/robots.txt
+
+echo "Removing the stuff you dont want..."
+
+
+echo "Initing the new git project..."
+
