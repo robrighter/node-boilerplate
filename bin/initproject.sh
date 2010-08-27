dd91dc542058706f99a94dc1dac3efb8bf82f875
Daniel Jackoway <danjdel@gmail.com>
2010-08-27 12:18:53 +0800

    get rid of all of the /project's in initproject.sh

--- bin/initproject.sh
+++ bin/initproject.sh
@@ -4,15 +4,15 @@ echo "Updating git submodules"
 git submodule update --init --recursive
 
 echo "Creating static folders"
-mkdir ./project/static/images
-mkdir ./project/static/css
+mkdir ./static/images
+mkdir ./static/css
 
 echo "Copying Markup and CSS BoilerPlate..."
-cp ./lib/html5-boilerplate/404.html ./project/views/404.ejs
-cp ./lib/html5-boilerplate/js/plugins.js ./project/static/js/plugins.js
-cp ./lib/html5-boilerplate/css/handheld.css ./project/static/css/handheld.css
-cp ./lib/html5-boilerplate/css/style.css ./project/static/css/style.css
-cp ./lib/html5-boilerplate/robots.txt ./project/static/robots.txt
+cp ./lib/html5-boilerplate/404.html ./views/404.ejs
+cp ./lib/html5-boilerplate/js/plugins.js ./static/js/plugins.js
+cp ./lib/html5-boilerplate/css/handheld.css ./static/css/handheld.css
+cp ./lib/html5-boilerplate/css/style.css ./static/css/style.css
+cp ./lib/html5-boilerplate/robots.txt ./static/robots.txt
 
 echo "Removing the stuff you dont want..."
 rm -rf .git
